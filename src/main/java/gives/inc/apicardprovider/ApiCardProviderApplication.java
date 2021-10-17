997f8a2476dc959641ec68af0238a2d7bfe33f2e
joao4018 <joao4018@gmail.com>
2021-10-16 22:11:08 -0300

    Config: init spring project

---
+++ src/main/java/gives/inc/apicardprovider/ApiCardProviderApplication.java
@@ -0,0 +1,13 @@
+package gives.inc.apicardprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCardProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCardProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gives/inc/apicardprovider/ApiCardProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package gives.inc.apicardprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCardProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
