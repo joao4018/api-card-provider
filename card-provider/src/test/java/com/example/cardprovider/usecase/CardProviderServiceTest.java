2869ee4f591b5d34794b15f245e6a367488ec31a
joao4018 <joao4018@gmail.com>
2021-10-17 11:55:38 -0300

    Delete: delete unused tests

--- card-provider/src/test/java/com/example/cardprovider/usecase/CardProviderServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.cardprovider.usecase;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-class CardProviderServiceTest {
-
-}
