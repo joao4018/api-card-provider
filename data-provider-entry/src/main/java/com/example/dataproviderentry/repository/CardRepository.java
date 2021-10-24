f32b720190ff139796484abfd00371b650c93c59
joao4018 <joao4018@gmail.com>
2021-10-23 23:02:02 -0300

    Config: schema and data up automatic

--- data-provider-entry/src/main/java/com/example/dataproviderentry/repository/CardRepository.java
+++ data-provider-entry/src/main/java/com/example/dataproviderentry/repository/CardRepository.java
@@ -5,9 +5,11 @@ import com.example.cardprovider.entity.impl.Card;
 import com.example.dataproviderentry.domain.CardEntity;
 import com.example.dataproviderentry.mapper.CardMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
+@Sql({"/data.sql"})
 public interface CardRepository extends JpaRepository<CardEntity, Long>, CardAdapterProvider {
     default List<Card> findAllCards() {
         return CardMapper.INSTANCE.toListCard(this.findAll());
---
+++ data-provider-entry/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+use apicard;
+INSERT INTO card_entity (id, card, description) VALUE(1,'lala', 'desc'),
+                                             (2,'lolo', 'desc');
